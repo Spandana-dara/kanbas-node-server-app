e6dbec13d0460c0399a70c78d14c207361cf117c
Spandana <spandana1286@gmail.com>
2023-11-13 19:41:31 -0500

    changed import to require

--- Database/index.js
+++ Database/index.js
@@ -1,11 +1,10 @@
-const courses = require('./courses.json');
-const modules = require('./modules.json');
-const assignments = require('./assignments.json');
-const users = require('./users.json');
-const grades = require('./grades.json');
-const enrollments = require('./enrollments.json');
-
-module.exports = {
+import courses from "./courses.json" assert { type: "json" };
+import modules from "./modules.json" assert { type: "json" };
+import assignments from "./assignments.json" assert { type: "json" };
+import users from "./users.json" assert { type: "json" };
+import grades from "./grades.json" assert { type: "json" };
+import enrollments from "./enrollments.json" assert { type: "json" };
+export default {
   courses,
   modules,
   assignments,
@@ -13,3 +12,5 @@ module.exports = {
   grades,
   enrollments,
 };
+
+
