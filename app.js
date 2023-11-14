c8b6ee774168d94783548b3a3c41498bfc1c988c
Spandana <spandana1286@gmail.com>
2023-11-13 19:14:03 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import Hello from "./hello.js";
+import Lab5 from "./Lab5.js";
+import cors from "cors";
+import CourseRoutes from "./courses/routes.js"
+import ModuleRoutes from "./modules/routes.js";
+import "dotenv/config";
+
+const app = express();
+app.use(express.json());
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  })
+);
+CourseRoutes(app);
+ModuleRoutes(app);
+Lab5(app)
+Hello(app)
+app.listen(process.env.PORT || 4000);
