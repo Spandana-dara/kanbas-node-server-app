4fa0d79e4ef507237479058bc1154f5a33a3602d
Spandana <spandana1286@gmail.com>
2023-12-02 17:31:07 -0500

    delete user

--- app.js
+++ app.js
@@ -5,13 +5,17 @@ import cors from "cors";
 import CourseRoutes from "./courses/routes.js"
 import ModuleRoutes from "./modules/routes.js";
 import "dotenv/config";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors()
 );
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas")
 CourseRoutes(app);
 ModuleRoutes(app);
 Lab5(app)
 Hello(app)
+UserRoutes(app)
 app.listen(process.env.PORT || 4000);
