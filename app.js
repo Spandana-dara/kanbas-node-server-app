a71be583bb0cdcf8936c46aef152074522eeb08c
Spandana <spandana1286@gmail.com>
2023-12-03 19:12:10 -0500

    added env

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: process.env.FRONTEND_URL,
+    //origin: process.env.FRONTEND_URL,
   })
 );
 const sessionOptions = {
