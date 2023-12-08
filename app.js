04d5c26c42bd7e97f44ccd5ec4e5dc26d8c645bc
Spandana <spandana1286@gmail.com>
2023-12-09 01:09:21 +0530

    courses

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ import Hello from "./hello.js"
 import Lab5 from "./Lab5.js"
 import cors from "cors";
 import bodyParser from "body-parser";
-import CourseRoutes from "./Courses/routes.js";
-import ModuleRoutes from "./Modules/routes.js";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from "./Assignments/routes.js";
 import mongoose from "mongoose";
 import UserRoutes from "./users/routes.js";
